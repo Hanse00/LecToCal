843b40ba034dc39e10369566dcaa3dcb2f2271d4
Philip Hansen <me@philiphansen.dk>
2016-03-16 14:20:52 +0100

    Argument Parsing
    Using the argparse module, argument parsing has been implemented,
    allowing the end user to specify as command line parameters, which file
    the google credentials should be read from, which user in lectio to
    sync with, and what to call the calendar in Google Calendar.

--- main.py
+++ main.py
@@ -1,8 +1,85 @@
-def main():
-    google_credentials = get_google_credentials()
-    schedule = get_lectio_schedule()
-    if schedule_has_changed(schedule, google_credentials):
-        update_google_calendar_with_schedule(schedule, google_credentials)
+# Copyright 2016 Philip Hansen
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-if __name__ = "__main__":
+import argparse
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description="Scrapes a Lectio schedule "
+                                     "and syncs it to Google Calendar.")
+    parser.add_argument("school_id",
+                        type=int,
+                        help="ID of the school user belongs to in Lectio")
+    parser.add_argument("user_type",
+                        choices=["student", "teacher"],
+                        help="User type in Lectio (options: student, teacher)")
+    parser.add_argument("user_id",
+                        type=int,
+                        help="User's ID in Lectio")
+    parser.add_argument("--credentials",
+                        default="storage.json",
+                        help="Path to the file storing the Google "
+                        "OAuth credentials (default: storage.json)")
+    parser.add_argument("--calendar",
+                        default="Lectio",
+                        help="Name to use for the calendar inside "
+                        "Google Calendar (default: Lectio)")
+
+    arguments = vars(parser.parse_args())
+    return arguments
+
+
+def get_google_credentials(credentials_store):
+    print("Getting the Google credentials at {}".format(credentials_store))
+    return "some_credentials"
+
+
+def get_lectio_schedule(school_id, user_type, user_id):
+    print("Getting schedule for user: {} {} at school: {}".format(user_type,
+                                                                  user_id,
+                                                                  school_id))
+    return {"class": "math"}
+
+
+def schedule_has_changed(google_credentials, calendar_name, schedule):
+    print("Checking if the schedule: {} is different from calendar: {} "
+          "for user with Google crendentials: {}".format(schedule,
+                                                         calendar_name,
+                                                         google_credentials))
+    return True
+
+
+def update_google_calendar_with_schedule(google_credentials,
+                                         calendar_name,
+                                         schedule):
+    print("Updating calendar: {}, with data: {}, for user with "
+          "crendentials: {}".format(calendar_name, schedule,
+                                    google_credentials))
+
+
+def main():
+    arguments = get_arguments()
+    google_credentials = get_google_credentials(arguments["credentials"])
+    schedule = get_lectio_schedule(arguments["school_id"],
+                                   arguments["user_type"],
+                                   arguments["user_id"])
+    if schedule_has_changed(google_credentials,
+                            arguments["calendar"],
+                            schedule):
+        update_google_calendar_with_schedule(google_credentials,
+                                             arguments["calendar"],
+                                             schedule)
+
+if __name__ == "__main__":
     main()
