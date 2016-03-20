b72f67701df4e0a5a49c5957489df8b4de649e54
Philip Hansen <me@philiphansen.dk>
2016-03-20 16:20:52 +0100

    Base for lesson parsing
    Set up the base functions for lesson parsing, two functions now need to
    be filled with appropriate functionality.

--- lectio.py
+++ lectio.py
@@ -14,6 +14,7 @@
 
 import datetime
 import requests
+from lxml import html
 
 
 class UserDoesNotExistError(Exception):
@@ -49,13 +50,13 @@ def _get_user_page(school_id, user_type, user_id, week=""):
     r = requests.get(URL_TEMPLATE.format(school_id,
                                          USER_TYPE[user_type],
                                          user_id,
-                                         ""),
+                                         week),
                      allow_redirects=False)
     return r
 
 
 def _get_lectio_weekformat_with_offset(offset):
-    today = datetime.date(2015, 12, 20)
+    today = datetime.date.today()
     future_date = today + datetime.timedelta(weeks=offset)
     week_number = "{0:02d}".format(future_date.isocalendar()[1])
     year_number = str(future_date.isocalendar()[0])
@@ -63,9 +64,40 @@ def _get_lectio_weekformat_with_offset(offset):
     return lectio_week
 
 
-def _get_week_schedule(school_id, user_type, user_id, week):
+def _get_id_from_link(link):
     pass
 
+
+def _get_info_from_title(title):
+    pass
+
+
+def _parse_element_to_lesson(element):
+    link = element.get("href")
+    id = None
+    if link:
+        id = _get_id_from_link(link)
+    summary, status, start_time, end_time = \
+        _get_details_from_title(element.get("title"))
+    return Lesson(id, summary, status, start_time, end_time)
+
+
+def _parse_page_to_lessons(page):
+    tree = html.fromstring(page)
+    # Find all a elements with class s2skemabrik in page
+    lesson_elements = tree.xpath("//a[contains(concat("
+                                 "' ', normalize-space(@class), ' '),"
+                                 "' s2skemabrik ')]")
+    lessons = []
+    for element in lesson_elements:
+        lessons += _parse_element_to_lesson(element)
+    return lessons
+
+
+def _retreive_week_schedule(school_id, user_type, user_id, week):
+    r = _get_user_page(school_id, user_type, user_id, week)
+    schedule = _parse_page_to_lessons(r.content)
+    return schedule
 
 
 def _filter_for_duplicates(schedule):
@@ -80,7 +112,10 @@ def _retreive_user_schedule(school_id, user_type, user_id):
     schedule = []
     for week_offset in range(4):
         week = _get_lectio_weekformat_with_offset(week_offset)
-        week_schedule = _get_week_schedule(school_id, user_type, user_id, week)
+        week_schedule = _retreive_week_schedule(school_id,
+                                                user_type,
+                                                user_id,
+                                                week)
         schedule += week_schedule
     filtered_schedule = _filter_for_duplicates(schedule)
     return filtered_schedule
