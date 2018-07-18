7d369536c394f5f340bee1b59db97951309592d1
Philip Peder Mallegol-Hansen <philip@mallegolhansen.com>
2018-07-18 12:30:26 +0100

    Absl flags
    Implements a basic example of using absl for flags.

---
+++ lectocal/main.py
@@ -0,0 +1,17 @@
+from absl import app
+from absl import flags
+
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_integer("cakes", 2, "How many cakes would you like?")
+
+
+def main(argv):
+    del argv
+
+    print("You opted for %d cake(s)." % FLAGS.cakes)
+
+
+if __name__ == "__main__":
+    app.run(main)
